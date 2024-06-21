42dc09612dbfd431bf92c04805dcd2763cef53c5
Stanislav <stankhil@gmail.com>
2024-06-21 08:44:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace _20._06
+{
+    internal class Program
+    {
+
+        static void Permute(char[] arr, int start, List<string> perm)
+        {
+            if (start == arr.Length - 1) perm.Add(new string(arr));
+            else
+            {
+                for (int i = start; i < arr.Length; i++)
+                {
+                    char temp = arr[start];
+                    arr[start] = arr[i];
+                    arr[i] = temp;
+                    Permute(arr, start + 1, perm);
+                    temp = arr[start];
+                    arr[start] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter string");
+            string input = Console.ReadLine();
+            List<string> perm = new List<string>();
+            Permute(input.ToCharArray(), 0, perm);
+
+            foreach (string p in perm)
+                Console.WriteLine(p);
+        }
+    }
+}
